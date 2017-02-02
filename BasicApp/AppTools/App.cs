1523d30c07649656060a76c50bc4a576a7ef3a4f
Victor <victor.pacheco@case-mate.com>
2017-02-02 16:29:55 -0500

    Create a controller called Shopify. This will be the entry point. So far this has only 1 action 'Install'. Also created a static class to pull the app config settings-check web.config for details(using file attr in appSettings

---
+++ BasicApp/AppTools/App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+namespace BasicApp.AppTools
+{
+    public static class App
+    {
+        public static string AppId = ConfigurationManager.AppSettings["AppId"];
+        public static string AppSecret = ConfigurationManager.AppSettings["AppSecret"];
+        public static string AppScope = ConfigurationManager.AppSettings["AppScope"];
+        public static string AppDomain = ConfigurationManager.AppSettings["AppDomain"];
+        public static string AppInstallControllerName = ConfigurationManager.AppSettings["AppInstallControllerName"];
+        public static string GetInstallState()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+    }
+}
---
+++ BasicApp/Controllers/ShopifyController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BasicApp.AppTools;
+namespace BasicApp.Controllers
+{
+    public class ShopifyController : Controller
+    {
+        //Step 1 - Shop wants to install app
+        public ActionResult Install(string shop)
+        {
+            //Note: In live production - check if this shop has already install the app - check other settings in case your app is not free - check for charge id is active/valid
+
+            //build redirect url
+            string redirectUrl = 
+                string.Format(
+                "https://{0}/admin/oauth/authorize?client_id={1}&scope={2}&redirect_uri=https://{3}/{4}/auth&state={5}",
+                shop,
+                App.AppId,
+                App.AppScope,
+                App.AppDomain,
+                App.AppInstallControllerName,
+                App.GetInstallState());
+
+            //Save GetInstallState value
+            var state = HttpUtility.ParseQueryString(redirectUrl).Get("state");
+            //save to database
+
+            return Redirect(redirectUrl);
+        }
+
+    }
+}
